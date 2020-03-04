d6828eda3b98c27201b41559971851509da5e400
Murph Murphy <murph@clurictec.com>
2020-03-04 13:40:44 -0700

    Initial attempt.

--- index.js
+++ index.js
@@ -1,22 +1,92 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const wait = require("./wait");
 
+async function github_query(github_token, query, variables) {
+  return fetch("https://api.github.com/graphql", {
+    method: "POST",
+    body: JSON.stringify({ query, variables }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `bearer ${github_token}`
+    }
+  }).then(function(response) {
+    return response.json();
+  });
+}
 
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const issue = core.getInput("issue");
+    const repository = core.getInput("repository");
+    const project = core.getInput("project");
+    const github_token = core.getInput("github_token");
+    const owner = repository.split("/")[0];
+    const name = repository.split("/")[1];
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const get_issue_id = `
+    query($owner:String!, $name:String!, $number:Int!){
+      repository(owner: $owner, name: $name) {
+        issue(number:$number) {
+          id
+        }
+      }
+    }`;
+    const issue_vars = {
+      owner,
+      name,
+      number: parseInt(issue)
+    };
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+    const issue_resp = await github_query(
+      github_token,
+      get_issue_id,
+      issue_vars
+    );
+    console.log(issue_resp);
+    const issue_id = issue_resp["data"]["repository"]["issue"]["id"];
+
+    const get_project_id = `
+    query($owner:String!, $name:String!, $number:Int!){
+      repository(owner: $owner, name: $name) {
+        project(number: $number) {
+          id
+        }
+      }
+    }`;
+    const project_vars = {
+      owner,
+      name,
+      number: parseInt(project)
+    };
+
+    const project_resp = await github_query(
+      github_token,
+      get_project_id,
+      project_vars
+    );
+    console.log(project_resp);
+    const project_id = project_resp["data"]["repository"]["project"]["id"];
+
+    console.log(`Adding issue ${issue} to project ${project}`);
+    console.log("");
+
+    query = `
+    mutation($issueId:ID!, $projectId:ID!) {
+      updateIssue(input:{id:$issueId, projectIds:[$projectId]}) {
+        issue {
+          id
+        }
+      }
+    }`;
+    variables = { issueId: issue_id, projectId: project_id };
+
+    response = await github_query(github_token, query, variables);
+    console.log(response);
+    console.log(`Done!`);
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
